3ec8d0e74b3bea88a2fc78b2608e13ea30bb3efe
brentbess21 <brentbess21@gmail.com>
2021-10-12 14:50:20 -0600

    Got the correct info coming back with the GET user by id endpoint

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -51,6 +51,10 @@ function findById(user_id) {
       "role_name": "instructor"
     }
    */
+    return db('users as u')
+    .leftJoin('roles as r', 'u.role_id', 'r.role_id')
+    .select('u.user_id', 'u.username', 'r.role_name')
+    .where('user_id', user_id)
 }
 
 /**
